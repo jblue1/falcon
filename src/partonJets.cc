37fa416e2f922af93b1e4f30ed80cf7cb07c68b5
jblue1 <john.s.blue@gmail.com>
2020-09-17 09:54:33 -0400

    works with helpers.h

--- src/partonJets.cc
+++ src/partonJets.cc
@@ -3,37 +3,13 @@
 #include "TTree.h"
 #include "TH1I.h"
 #include "TH1F.h"
-#include <cmath>
 #include <fstream>
 #include <cassert>
 #include <string>
-#include <numeric>
 #include <iomanip>
+#include "helpers.h"
 
 using namespace fastjet;
-
-/**
- * Calculate the average value of a vector
- */
-float vecAvg(std::vector<float> &vec) {
-    float numerator = std::accumulate(vec.begin(), vec.end(), 0.0);
-    float denominator = (float) vec.size();
-    return numerator/denominator;
-}
-
-/**
- * Calculate the difference between two angles in the range (-pi, pi).
- */
-float angleDif(float angle1,float angle2) {
-    return atan2(sin(angle1-angle2), cos(angle1-angle2));
-}
-
-/**
- * Calculate delta R for two points in eta phi space.
- */
-float deltaR(float eta1, float phi1, float eta2, float phi2) {
-    return sqrt( pow(eta2 - eta1, 2) + pow(angleDif(phi2, phi1), 2));
-}
 
 /**
  * Describe parameters taken and print errors if parameters are incorrectly
