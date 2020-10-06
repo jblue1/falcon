9f9713cb0ed81b4a32b6577a0acb35b4c51290fc
jblue1 <john.s.blue@gmail.com>
2020-10-06 08:44:27 -0400

    one-hot encode pdgId

--- src/makeData.cc
+++ src/makeData.cc
@@ -24,12 +24,13 @@ int main(int arc, char const *argv[]) {
     assert(write_out.is_open());
 
     write_out << "The file contains 4 vectors of matched parton and gen jets. The columns are" << std::endl;
-    write_out << "Px_p Py_p Pz_p E_p Px_g Py_g Pz_g E_g" << std::endl;
+    write_out << "Px_p Py_p Pz_p E_p tbar bbar cba sbar ubar dbar d u s c b t g Px_g Py_g Pz_g E_g" << std::endl;
 
     std::vector<Float_t>* partonPx = 0;
     std::vector<Float_t>* partonPy = 0;
     std::vector<Float_t>* partonPz = 0;
     std::vector<Float_t>* partonE = 0;
+    std::vector<Int_t>* partonPdgId = 0;
     ULong64_t partonEvent = 0;
 
     std::vector<Float_t> *genJetPt = 0;
@@ -46,6 +47,7 @@ int main(int arc, char const *argv[]) {
     tree1->SetBranchAddress("genPartPz", &partonPz);
     tree1->SetBranchAddress("genPartE", &partonE);
     tree1->SetBranchAddress("event", &partonEvent);
+    tree1->SetBranchAddress("genPartPdgId", &partonPdgId);
 
     tree3->SetBranchAddress("genJetPt", &genJetPt);
     tree3->SetBranchAddress("genJetEta", &genJetEta);
@@ -61,6 +63,19 @@ int main(int arc, char const *argv[]) {
     for (int i=0; i < numEvents; i++) {
         tree1->GetEntry(i);
         tree3->GetEntry(i);
+        int tbar = 0;
+        int bbar = 0;
+        int cbar = 0;
+        int sbar = 0;
+        int ubar = 0;
+        int dbar = 0;
+        int d = 0;
+        int u = 0;
+        int s = 0;
+        int c = 0;
+        int b = 0;
+        int t = 0;
+        int g = 0;
 
         assert(genJetEvent == partonEvent);
 
@@ -68,6 +83,7 @@ int main(int arc, char const *argv[]) {
         std::vector<PseudoJet> partons;
         for (int j=0; j < numPartons; j++) {
             partons.push_back(PseudoJet( (*partonPx)[j], (*partonPy)[j], (*partonPz)[j], (*partonE)[j]));
+            partons[j].set_user_index(j);
         }
 
         double R = 0.4;
@@ -77,13 +93,36 @@ int main(int arc, char const *argv[]) {
         std::vector<PseudoJet> partonJets = cs.inclusive_jets();
 
         if (partonJets.size() > 0) {
-            for (int j=0; j< partonJets.size(); j++) {
+            for (size_t j=0; j< partonJets.size(); j++) {
                 if (partonJets[j].pt() > 20) {
+                    std::vector<PseudoJet> constituents = sorted_by_pt(partonJets[j].constituents());
+                    int index = constituents[0].user_index();
+                    std::cout << "Highest Pt PdgId: " << (*partonPdgId)[index] << std::endl;
+                    int pdgId = (*partonPdgId)[index];
+                    
+                    if (pdgId == -6) tbar = 1;
+                    else if (pdgId == -5) bbar = 1;
+                    else if (pdgId == -4) cbar = 1;
+                    else if (pdgId == -3) sbar = 1;
+                    else if (pdgId == -2) ubar = 1;
+                    else if (pdgId == -1) dbar = 1;
+                    else if (pdgId == 1) d = 1;
+                    else if (pdgId == 2) u = 1;
+                    else if (pdgId == 3) s = 1;
+                    else if (pdgId == 4) c = 1;
+                    else if (pdgId == 5) b = 1;
+                    else if (pdgId == 6) t = 1;
+                    else if (pdgId == 21) g = 1;
+                    else {
+                        std::cout << "Unknown Pdgid: " << pdgId << std::endl;
+                        exit(1);
+                    }
+
                     float partonEta = partonJets[j].rap();
                     float partonPhi = partonJets[j].phi_std();
                     float minDR = 10.0;
                     int jetIndex = 0;
-                    for (int k=0; k < genJetPt->size(); k++) {
+                    for (size_t k=0; k < genJetPt->size(); k++) {
                         if ((*genJetPt)[k] > 30) {
                             float dR = deltaR((*genJetEta)[k], (*genJetPhi)[k], partonEta, partonPhi);
                             if (dR < minDR) {
@@ -93,16 +132,34 @@ int main(int arc, char const *argv[]) {
                         }
                     }
                     if (minDR < 0.35) {
-                        write_out << partonJets[j].pt() << " " << partonJets[j].rap() << " " << partonJets[j].phi_std()
-                            << " " <<  partonJets[j].e() << " " << (*genJetPt)[jetIndex] << " "
-                            << (*genJetEta)[jetIndex] << " " << (*genJetPhi)[jetIndex] << " "
+                        write_out 
+                            << partonJets[j].pt() << " " 
+                            << partonJets[j].rap() << " " 
+                            << partonJets[j].phi_std() << " " 
+                            << partonJets[j].e() << " " 
+                            << tbar << " "
+                            << bbar << " "
+                            << cbar << " "
+                            << sbar << " "
+                            << ubar << " "
+                            << dbar << " "
+                            << d << " "
+                            << u << " "
+                            << s << " "
+                            << c << " "
+                            << b << " "
+                            << t << " "
+                            << g << " "
+                            << (*genJetPt)[jetIndex] << " "
+                            << (*genJetEta)[jetIndex] << " " 
+                            << (*genJetPhi)[jetIndex] << " "
                             << (*genJetE)[jetIndex] << std::endl;
-                        std::cout << "Parton Pt: " << partonJets[j].pt() << std::endl;
-                        std::cout << "Gen Pt: " << (*genJetPt)[jetIndex] << std::endl;
-                        std::cout << "Parton Eta: " << partonJets[j].rap() << std::endl;
-                        std::cout << "Gen Eta: " << (*genJetEta)[jetIndex] << std::endl;
-                        std::cout << "Parton Phi: " << partonJets[j].phi() << std::endl;
-                        std::cout << "Gen Phi: " << (*genJetPhi)[jetIndex] << std::endl;
+                        //std::cout << "Parton Pt: " << partonJets[j].pt() << std::endl;
+                        //std::cout << "Gen Pt: " << (*genJetPt)[jetIndex] << std::endl;
+                        //std::cout << "Parton Eta: " << partonJets[j].rap() << std::endl;
+                        //std::cout << "Gen Eta: " << (*genJetEta)[jetIndex] << std::endl;
+                        //std::cout << "Parton Phi: " << partonJets[j].phi() << std::endl;
+                        //std::cout << "Gen Phi: " << (*genJetPhi)[jetIndex] << std::endl;
                         }
                     }
                 }
