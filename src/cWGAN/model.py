5de115a8be8a95f5a82d1d07ba45217c4d4ae6b2
jblue1 <john.s.blue@gmail.com>
2020-12-02 13:12:35 -0500

    change params for initializing wGAN object

--- src/cWGAN/model.py
+++ src/cWGAN/model.py
@@ -5,26 +5,26 @@ from tensorflow.keras.constraints import max_norm
 
 
 class weight_clipping(tf.keras.constraints.Constraint):
+    '''
+    TODO: implement as way of clipping weights in network
+    '''
+
     def __init__(self, ref_value):
         self.ref_value = ref_value
 
 
 
 class cWGAN():
-    def __init__(self, dataset, num_batches):
+    def __init__(self, num_critic_iters, batch_size):
         # hyper parameters recommended by paper
-        self.num_critic_iters = 5
-        self.clip_value = 0.01
+        self.num_critic_iters = num_critic_iters
+        self.clip_value = 0.1
         self.critic_optimizer = tf.keras.optimizers.RMSprop(lr=5e-5)
         self.generator_optimizer = tf.keras.optimizers.RMSprop(lr=5e-5)
-        self.batch_size = 64 
+        self.batch_size = batch_size 
 
         self.generator = self.build_generator()
         self.critic = self.build_critic()
-        self.dataset = dataset.batch(self.batch_size)
-        self.dataset = self.dataset.shuffle(num_batches,
-                reshuffle_each_iteration=True)
-        
 
 
     def build_generator(self):
@@ -33,16 +33,16 @@ class cWGAN():
 
         
         x = keras.layers.Dense(32, activation='relu', name="genx1")(pJet)
-        x = keras.layers.Dense(32, activation='relu', name="genx2")(x)
-        x = keras.layers.Dense(32, activation='relu', name="genx3")(x)
+        x = keras.layers.Dense(64, activation='relu', name="genx2")(x)
+        x = keras.layers.Dense(64, activation='relu', name="genx3")(x)
 
         y = keras.layers.Dense(32, activation='relu', name="geny1")(noise)
-        y = keras.layers.Dense(32, activation='relu', name="geny2")(y)
-        y = keras.layers.Dense(32, activation='relu', name="geny3")(y)
+        y = keras.layers.Dense(64, activation='relu', name="geny2")(y)
+        y = keras.layers.Dense(64, activation='relu', name="geny3")(y)
 
         concat = keras.layers.concatenate([x, y], name="concat")
-        out = keras.layers.Dense(32, activation='relu', name="both1")(concat)
-        out = keras.layers.Dense(32, activation='relu', name="both2")(concat)
+        out = keras.layers.Dense(64, activation='relu', name="both1")(concat)
+        out = keras.layers.Dense(64, activation='relu', name="both2")(concat)
         out = keras.layers.Dense(4, name="out")(out)
         return keras.Model([pJet, noise], out)
    
@@ -52,16 +52,16 @@ class cWGAN():
         rJet = keras.Input(shape=(4,))
 
         x = keras.layers.Dense(32, activation='relu')(pJet)
-        x = keras.layers.Dense(32, activation='relu')(x)
-        x = keras.layers.Dense(32, activation='relu')(x)
+        x = keras.layers.Dense(64, activation='relu')(x)
+        x = keras.layers.Dense(64, activation='relu')(x)
 
         y = keras.layers.Dense(32, activation='relu')(rJet)
-        y = keras.layers.Dense(32, activation='relu')(y)
-        y = keras.layers.Dense(32, activation='relu')(y)
+        y = keras.layers.Dense(64, activation='relu')(y)
+        y = keras.layers.Dense(64, activation='relu')(y)
 
         concat = keras.layers.concatenate([x, y])
-        out = keras.layers.Dense(32, activation='relu')(concat)
-        out = keras.layers.Dense(32, activation='relu')(concat)
+        out = keras.layers.Dense(128, activation='relu')(concat)
+        out = keras.layers.Dense(128, activation='relu')(concat)
         out = keras.layers.Dense(1)(out)
         return keras.Model([pJet, rJet], out)
 
@@ -71,6 +71,7 @@ class cWGAN():
         self.critic.summary()
 
 
+    #@tf.function
     def critic_loss(self, real_output, fake_output):
         '''
         The negative of the estimate of the wasserstein distance (negative
@@ -82,20 +83,24 @@ class cWGAN():
         return loss
 
 
+    #@tf.function
     def generator_loss(self, fake_output):
         loss = -tf.math.reduce_mean(fake_output)
         #print("Gen Loss: {}".format(loss.numpy()))
         return loss
 
          
+    #@tf.function
     def clip_critic_weights(self):
         for l in self.critic.layers:
-            weights = l.get_weights()
-            weights = [np.clip(w, -self.clip_value, self.clip_value) for w in weights]
-            l.set_weights(weights)
+            new_weights = []
+            for i in range(len(l.weights)):
+                new_weights.append(tf.clip_by_value(l.weights[i],
+                    -self.clip_value, self.clip_value))
+            l.set_weights(new_weights)
+                
 
-
-    #@tf.function()
+    #@tf.function
     def train_critic(self, pJets, rJets):
         shape = tf.shape(pJets)
         noise = tf.random.uniform(shape, 0, 1, tf.float32)
@@ -107,8 +112,8 @@ class cWGAN():
                     training=True)
 
             critic_loss = self.critic_loss(real_output, fake_output)
-
         
+        #print("    Critic Loss: {}".format(critic_loss))
         critic_grads = tape.gradient(critic_loss,
                 self.critic.trainable_variables)
 
@@ -118,7 +123,7 @@ class cWGAN():
         self.clip_critic_weights()
 
 
-    #@tf.function()
+    #@tf.function
     def train_generator(self, pJets):
         shape = tf.shape(pJets)
         noise = tf.random.uniform(shape, 0, 1, tf.float32)
@@ -134,9 +139,10 @@ class cWGAN():
             self.generator.trainable_variables))
 
 
-    def train_step(self):
-        data = self.dataset.take(self.num_critic_iters + 2)
+    #@tf.function
+    def train_step(self, data):
         count = 0
+        wass_estimate = 0.0
         for batch in data:
             if count < self.num_critic_iters:
                 self.train_critic(batch[0], batch[1])
@@ -145,26 +151,16 @@ class cWGAN():
             if count == self.num_critic_iters + 1:
                 pJets = batch[0]
                 rJets = batch[1]
-                #print("PJets")
-                #print(pJets)
-                #print("rJets")
-                #print(rJets)
                 shape = tf.shape(pJets)
                 noise = tf.random.uniform(shape, 0, 1, tf.float32)
 
                 generated_rJets = self.generator([pJets, noise], training=False)
-                #print("Gen rJets")
-                #print(generated_rJets)
                 real_output = self.critic([pJets, rJets], training=False)
                 fake_output = self.critic([pJets, generated_rJets], training=False)
-                #print(real_output)
-                #print(fake_output)
                 wass_estimate = -self.critic_loss(real_output, fake_output)
             count += 1
 
-        return wass_estimate.numpy()
-
-
+        return wass_estimate
 
 
 def main():
@@ -197,7 +193,7 @@ def main():
     train_dataset = tf.data.Dataset.from_tensor_slices((trainParton,
             trainPf))
 
-    net = cWGAN(train_dataset, 100)
+    net = cWGAN(5, 64)
     net.clip_critic_weights()
 
 
