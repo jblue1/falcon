c055b4565dcbc84ec4465af47c1386c633653323
jblue1 <john.s.blue@gmail.com>
2020-12-08 15:43:55 -0500

    clean up:

--- src/cWGAN/train_mnist.py
+++ src/cWGAN/train_mnist.py
@@ -16,7 +16,41 @@ def one_hot_encode(number):
     return label
 
 
+def load_data():
+    """
+    Load and normalize MNIST data
+    returns - tf.data.Dataset object
+    returns - number of training examples
+    """
+    (train_images, train_labels), (_, _) = keras.datasets.mnist.load_data()
+    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+    train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
+
+    one_hot_labels = np.zeros((train_labels.shape[0], 10))
+    for i in range(len(train_labels)):
+        one_hot_labels[i, :] = one_hot_encode(train_labels[i])
+
+    dataset = tf.data.Dataset.from_tensor_slices((one_hot_labels, train_images))
+    return dataset, train_labels.shape[0]
+
+
+def print_network(save_dir, net):
+    """
+    Print network layers to text file in save directory
+    save_dir - location to save file
+    net - cWGAN object 
+    """
+
+    fname = os.path.join(save_dir, 'modelsummary.txt')
+    with open(fname, 'w') as f:
+        with redirect_stdout(f):
+            net.print_network()
+
+
 def main():
+    
+    save_dir = train.make_save_directory()
+    '''
     today = str(date.today())
     run_number = 0
     save_dir = '../../models/cWGAN/Run_MNIST_' + today + '_' + str(run_number) 
@@ -28,36 +62,27 @@ def main():
     print("SAVE DIR: " + save_dir)
     os.makedirs(save_dir)
     assert(os.path.isdir(save_dir))
+    '''
 
-
-    (train_images, train_labels), (_, _) = keras.datasets.mnist.load_data()
-    train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
-    train_images = (train_images - 127.5) / 127.5 # Normalize the images to [-1, 1]
-
-    one_hot_labels = np.zeros((train_labels.shape[0], 10))
-    for i in range(len(train_labels)):
-        one_hot_labels[i, :] = one_hot_encode(train_labels[i])
-
-    dataset = tf.data.Dataset.from_tensor_slices((one_hot_labels, train_images))
-
-    print(dataset)
+    dataset, num_data_points = load_data()
+    
     cwgan = model.cWGAN_mnist(train.NUM_CRITIC_ITERS, train.BATCH_SIZE,
             noise_dims=100)
-    fname = os.path.join(save_dir, 'modelsummary.txt')
-    with open(fname, 'w') as f:
-        with redirect_stdout(f):
-            cwgan.print_network()
-
-
+    
+    print_network(save_dir, cwgan)
     start = time.time()
-    losses, critic_losses = train.train(cwgan, dataset, train.NUM_EPOCHS, train_labels.shape[0], save_dir)
+    generator_losses, critic_losses = train.train(cwgan, dataset,
+            train.NUM_EPOCHS, num_data_points, save_dir)
     print("Time for {} epochs: {}".format(train.NUM_EPOCHS, time.time() - start))
+    train.save_losses(save_dir, generator_losses, critic_losses)
+    '''
     filename = save_dir + '/losses.txt'
     critic_filename = save_dir + '/critic_losses.txt'
     loss_df = pd.DataFrame({"Wass Est": pd.Series(losses)})
     loss_df.to_csv(filename, header='None', index='None', sep=' ')
     critic_loss_df = pd.DataFrame({"Critic_loss": pd.Series(critic_losses)})
     critic_loss_df.to_csv(critic_filename, header='None', index='None', sep=' ')
+    '''
 
 
 if __name__ == '__main__':
