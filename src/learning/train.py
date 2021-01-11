59cd7f607891ab483a9d50f1568027afac3d1245
jblue1 <joblue@davidson.edu>
2021-01-11 13:23:26 -0500

    Save weights, losses, and model for cWGAN for MNIST data

--- src/learning/train.py
+++ src/learning/train.py
@@ -9,49 +9,54 @@ import time
 import numpy as np
 
 
-class FCNNTrainer():
+class FCNNTrainer:
     def __init__(self, lr, epochs, data_path, batch_size):
         self.save_dir = file_utils.make_save_directory("FCNN")
         self.model = FCNN.make_model()
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
-           loss=tf.keras.losses.MeanAbsoluteError(),
-           metrics=[tf.keras.metrics.MeanAbsoluteError()])
+        self.model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
+            loss=tf.keras.losses.MeanAbsoluteError(),
+            metrics=[tf.keras.metrics.MeanAbsoluteError()],
+        )
 
         self.batch_size = batch_size
         self.parton_data, self.reco_data = data_utils.load_jet_data(data_path)
-        self.checkpoint_path = os.path.join(self.save_dir, 'training/cp.cpkt')
+        self.checkpoint_path = os.path.join(self.save_dir, "training/cp.cpkt")
         self.epochs = epochs
 
-
     def train(self):
-        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=self.checkpoint_path,
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=self.checkpoint_path,
             save_weights_only=True,
             verbose=1,
-            save_best_only=True)
+            save_best_only=True,
+        )
 
-        history = self.model.fit(self.parton_data,
+        history = self.model.fit(
+            self.parton_data,
             self.reco_data,
             batch_size=self.batch_size,
             epochs=self.epochs,
             validation_split=0.2,
-            callbacks=[cp_callback])
+            callbacks=[cp_callback],
+        )
 
         return history
 
-
     def save_losses(self, history):
-        training_loss = history.history['loss']
-        val_loss = history.history['val_loss']
-        loss_dict = {'Training Loss' : training_loss, 'Validation Loss' : val_loss}
+        training_loss = history.history["loss"]
+        val_loss = history.history["val_loss"]
+        loss_dict = {"Training Loss": training_loss, "Validation Loss": val_loss}
         file_utils.save_losses(self.save_dir, loss_dict)
-
 
     def save_model(self):
         file_utils.save_network(self.save_dir, model_path="./FCNN.py")
 
 
-class cWGANTrainer():
-    def __init__(self, data_path, num_critic_iters, batch_size, lr, epochs):
+class cWGANTrainer:
+    def __init__(
+        self, data_path, num_critic_iters, batch_size, lr, epochs, save_interval
+    ):
         self.save_dir = file_utils.make_save_directory("cWGAN")
         self.num_critic_iters = num_critic_iters
         self.batch_size = batch_size
@@ -59,38 +64,88 @@ class cWGANTrainer():
         self.data = data_utils.load_mnist_data()
         self.epochs = epochs
         self.num_training_examples = len(self.data[0])
+        self.weight_saving_interval = save_interval
 
+    def sample_batch_of_data(self):
+        indices = np.random.choice(
+            np.arange(self.num_training_examples), self.batch_size, replace=False
+        )
+        labels = self.data[1][indices]
+        images = self.data[2][indices]
+        return labels, images
 
     def train(self):
-        batches_per_epoch = self.num_training_examples // self.batch_size 
+        """
+        Training loop for the cWGAN. An "epoch" is considered to be when the generator
+        has seen the same number of examples as are in the data set (note that since the
+        batches are randomly sampled, it won't actually get trained on all the data
+        each epoch).
+        """
+        critic_losses = []
+        generator_losses = []
+        wass_estimates = []
+        batches_per_epoch = self.num_training_examples // self.batch_size
         for epoch in range(self.epochs):
             start = time.time()
-
-            for batch_number in range(5): #range(batches_per_epoch):
-
+            for batch_number in range(5):  # range(batches_per_epoch):
                 # train critic for num_critic_iters
                 for critic_iter in range(self.num_critic_iters):
-                    indices = np.random.choice(np.arange(self.num_training_examples), self.batch_size, replace=False)
-                    labels = self.data[1][indices]
-                    images = self.data[2][indices]
-
+                    labels, images = self.sample_batch_of_data()
                     critic_loss = self.model.train_critic(labels, images)
+                    critic_losses.append(critic_loss)
                     self.model.clip_critic_weights()
-                    print("Critic loss {}".format(critic_loss))
-                
                 # train generator
-                indices = np.random.choice(np.arange(self.num_training_examples), self.batch_size, replace=False)
-                labels = self.data[1][indices]
-                self.model.train_generator(labels)
-                
+                labels, images = self.sample_batch_of_data()
+                generator_loss = self.model.train_generator(labels)
+                predicted_images = self.model.make_generator_predictions(labels)
+                generator_losses.append(generator_loss)
+                real_output = self.model.critic([labels, images], training=False)
+                fake_output = self.model.critic([labels, predicted_images], training=False)
+                wass_estimate = -self.model.critic_loss(real_output, fake_output)
+                wass_estimates.append(wass_estimate)
+                iteration = epoch * batches_per_epoch + batch_number
+                if iteration % self.weight_saving_interval == 0:
+                    self.save_weights(iteration)
+                print(
+                    "Iteration: {}  Wasserstein Estimate: {}".format(
+                        iteration, wass_estimate
+                    )
+                )
+            print("Time for epoch {}: {:1f}s".format(epoch, time.time() - start))
+        return critic_losses, generator_losses, wass_estimates
+
+    def save_weights(self, iteration):
+        checkpoint_dir = self.save_dir + "/training_checkpoints"
+        gen_filename = os.path.join(checkpoint_dir, "gen_" + str(iteration))
+        critic_filename = os.path.join(checkpoint_dir, "critic_" + str(iteration))
+        print("Saving generator weights at {}".format(gen_filename))
+        self.model.generator.save_weights(gen_filename)
+        self.model.critic.save_weights(critic_filename)
+
+    def save_losses(self, critic_losses, generator_losses, wass_estimates):
+        critic_loss_dict = {
+            "Critic Loss": critic_losses,
+        }
+        file_utils.save_losses(self.save_dir, critic_loss_dict, "critic_")
+        generator_loss_dict = {
+            "Generator Loss": generator_losses,
+            "Wasserstein Estimates": wass_estimates,
+        }
+        file_utils.save_losses(self.save_dir, generator_loss_dict, "generator_")
 
 
-
+    def save_model(self):
+        file_utils.save_network(self.save_dir, model_path="./cWGAN.py")
 
 
 def train_fcnn(params):
     print("Training fcnn")
-    trainer = FCNNTrainer(lr=1e-4, epochs=1, data_path='../../data/processed/newPartonMatchedJets.txt', batch_size=64)
+    trainer = FCNNTrainer(
+        lr=1e-4,
+        epochs=1,
+        data_path="../../data/processed/newPartonMatchedJets.txt",
+        batch_size=64,
+    )
     history = trainer.train()
     trainer.save_losses(history)
     trainer.save_model()
@@ -102,9 +157,17 @@ def train_cGAN(params):
 
 def train_cWGAN(params):
     print("Training cWGAN")
-    trainer = cWGANTrainer(data_path="../../data/processed/newPartonMatchedJets.txt", num_critic_iters=5,
-    batch_size=64, lr=1e-5, epochs=1)
-    trainer.train()
+    trainer = cWGANTrainer(
+        data_path="../../data/processed/newPartonMatchedJets.txt",
+        num_critic_iters=5,
+        batch_size=64,
+        lr=1e-5,
+        epochs=1,
+        save_interval=2,
+    )
+    critic_losses, generator_losses, wass_estimates = trainer.train()
+    trainer.save_losses(critic_losses, generator_losses, wass_estimates)
+    trainer.save_model()
 
 
 def train(model, params):
