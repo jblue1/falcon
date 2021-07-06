307a2feacb148b42d28ecf066d0fd1da115d4e61
joblue <joblue@davidson.edu@phy1kuchera.davidson.edu>
2021-07-06 15:43:05 -0400

    Change fourth feature from energy to mass

--- src/writeJetMomentaAngular.cc
+++ src/writeJetMomentaAngular.cc
@@ -44,11 +44,17 @@ int main(int arc, char const *argv[])
     std::vector<Float_t> *pfJetPt = 0;
     std::vector<Float_t> *pfJetEta = 0;
     std::vector<Float_t> *pfJetPhi = 0;
+    std::vector<Float_t> *pfJetPx = 0;
+    std::vector<Float_t> *pfJetPy = 0;
+    std::vector<Float_t> *pfJetPz = 0;
     std::vector<Float_t> *pfJetE = 0;
 
     tree->SetBranchAddress("pfJetPt", &pfJetPt);
     tree->SetBranchAddress("pfJetEta", &pfJetEta);
     tree->SetBranchAddress("pfJetPhi", &pfJetPhi);
+    tree->SetBranchAddress("pfJetPx", &pfJetPx);
+    tree->SetBranchAddress("pfJetPy", &pfJetPy);
+    tree->SetBranchAddress("pfJetPz", &pfJetPz);
     tree->SetBranchAddress("pfJetE", &pfJetE);
 
     tree->SetBranchAddress("genJetPt", &genJetPt);
@@ -63,6 +69,7 @@ int main(int arc, char const *argv[])
     tree->SetBranchAddress("partonStatus", &partonStatus);
 
     int numEvents = tree->GetEntries();
+    int numJets = 0;
 
     for (int i = 0; i < numEvents; i++)
     {
@@ -92,32 +99,40 @@ int main(int arc, char const *argv[])
                     int pfJetIndex = 0;
                     for (size_t k = 0; k < pfJetPt->size(); k++)
                     {
-                            float dR = deltaR((*pfJetEta)[k], (*pfJetPhi)[k], partonJets[j].rap(),
-                                              partonJets[j].phi_std());
-                            if (dR < minDRPfJet)
-                            {
-                                minDRPfJet = dR;
-                                pfJetIndex = k;
-                            }
+                        float dR = deltaR((*pfJetEta)[k], (*pfJetPhi)[k], partonJets[j].rap(),
+                                          partonJets[j].phi_std());
+                        if (dR < minDRPfJet)
+                        {
+                            minDRPfJet = dR;
+                            pfJetIndex = k;
+                        }
                     }
 
                     if (minDRPfJet < 0.35)
                     {
-                        write_out
-                            << partonJets[j].pt() << " "
-                            << partonJets[j].eta() << " "
-                            << partonJets[j].phi_std() << " "
-                            << partonJets[j].e() << " "
-                            << (*pfJetPt)[pfJetIndex] << " "
-                            << (*pfJetEta)[pfJetIndex] << " "
-                            << (*pfJetPhi)[pfJetIndex] << " "
-                            << (*pfJetE)[pfJetIndex] << std::endl;
+                        float partonJetPSquared = pow(partonJets[j].px(), 2) + pow(partonJets[j].py(), 2) + pow(partonJets[j].pz(), 2);
+                        float pfJetPSquared = pow((*pfJetPx)[pfJetIndex], 2) + pow((*pfJetPy)[pfJetIndex], 2) + pow((*pfJetPz)[pfJetIndex], 2);
+                        float partonJetMSquared = pow(partonJets[j].e(), 2) - partonJetPSquared;
+                        float pfJetMSquared = pow((*pfJetE)[pfJetIndex], 2) - pfJetPSquared;
+                        if (partonJetMSquared > 0 && pfJetMSquared > 0)
+                        {
+                            numJets++;
+                            write_out
+                                << partonJets[j].pt() << " "
+                                << partonJets[j].eta() << " "
+                                << partonJets[j].phi_std() << " "
+                                << sqrt(partonJetMSquared) << " "
+                                << (*pfJetPt)[pfJetIndex] << " "
+                                << (*pfJetEta)[pfJetIndex] << " "
+                                << (*pfJetPhi)[pfJetIndex] << " "
+                                << sqrt(pfJetMSquared) << std::endl;
+                        }
                     }
                 }
             }
         }
     }
-
+    std::cout << "Wrote " << numJets << " to " << txtPath << std::endl;
     write_out.close();
 
     return 0;
