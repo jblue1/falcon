1e22091aafba82b1a639f175e88bd51539e8a673
jblue1 <john.s.blue98@gmail.com>
2022-08-03 22:02:26 -0400

    Save progress - initial version still not done

--- src/data_generation/process_delphes_output.cc
+++ src/data_generation/process_delphes_output.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "TClonesArray.h"
+#include "TChain.h"
 #include "ExRootAnalysis/ExRootTreeReader.h"
 #include "classes/DelphesClasses.h"
 int main(int argc, char const *argv[]) {
@@ -9,6 +11,18 @@ int main(int argc, char const *argv[]) {
     ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
     Long64_t numDelphesEvents = treeReader->GetEntries();
     TClonesArray *branchRecoJet = treeReader->UseBranch("Jet");
+
+    for (int i = 0; i < numDelphesEvents; i++) {
+        treeReader->ReadEntry(i);
+
+        int numJets = branchRecoJet->GetEntries();
+        std::cout << "There were " << numJets << " jets in the event" << std::endl;
+        for (int j = 0; j < numJets; j++ ) {
+            Jet *jet = (Jet*) branchRecoJet->At(i);
+            std::cout << "Jet pt: " << jet->PT << std::endl;
+        }
+
+    }
 
     delete treeReader;
     return 0;
